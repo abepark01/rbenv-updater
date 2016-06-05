4d902b7a38b05af9409f071956186f2d7535a4ab
Abe Park <abepark01@gmail.com>
2016-06-04 17:56:51 -0700

    script to update rbenv

---
+++ update_rbenv.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+home = ENV['HOME']
+rbenv_dir = "#{home}/.rbenv"
+Dir.chdir(rbenv_dir) do
+  puts "Updating rbenv..."
+  puts `git pull`
+end
+
+rubybuild_dir = "#{rbenv_dir}/plugins/ruby-build"
+Dir.chdir(rubybuild_dir) do
+  puts "Updating ruby-build..."
+  puts `git pull`
+end
